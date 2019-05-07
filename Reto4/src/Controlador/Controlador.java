1febdeadc754fdebe1030c473d8b2318e2e388d8
YaroslavDemyanenko <yaroslav.demyanenko@elorrieta-errekamari.com>
2019-05-07 12:55:44 +0200

    Juntar

--- Reto4/src/Controlador/Controlador.java
+++ Reto4/src/Controlador/Controlador.java
@@ -40,27 +40,26 @@ public class Controlador {
 		});
 		vista.eleccion.getComboBox_1().addItemListener(new ItemListener() {
 			public void itemStateChanged(ItemEvent e) {
-				SentenciasBBDD sentencias = new SentenciasBBDD();
+				// SentenciasBBDD sentencias = new SentenciasBBDD();
 				System.out.println(e.getItem().toString());
 				ubicacion = e.getItem().toString();
-				ArrayList<Hotel> a = sentencias.visualizarCiudad(ubicacion);
+				// ArrayList<Hotel> a = sentencias.visualizarCiudad(ubicacion);
 
 			}
 		});
+		// La accion es que mete a la tabla con la ubicacion que pase del combobox
 		vista.eleccion.getbtnBuscar().addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				vista.mostrarPanel(vista.listado);
 				vista.listado.getTable().setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 				DefaultTableModel model = new DefaultTableModel();
-					
-				String ubicacion = (String) vista.eleccion.getComboBox_1().getSelectedItem();
+				// String ubicacion = (String) vista.eleccion.getComboBox_1().getSelectedItem();
 				SentenciasBBDD sentencias = new SentenciasBBDD();
 				ArrayList<Hotel> hotel1 = sentencias.visualizarCiudad(ubicacion);
-
-				Object[] columnas = new Object[3];
+				Object[] columnas = new Object[2];
 				columnas[0] = "Nombre";
 				columnas[1] = "Precio";
-				columnas[2] = "";
+		
 				model.setColumnIdentifiers(columnas);
 				Object[] filas = new Object[2];
 				for (int i = 0; i < hotel1.size(); i++) {
@@ -71,13 +70,12 @@ public class Controlador {
 					System.out.println(filas[1]);
 					model.addRow(filas);
 				}
-
 				vista.listado.getTable().setModel(model);
 				vista.listado.getScrollPane().setViewportView(vista.listado.getTable());
 
 			}
 		});
-		
+
 		vista.eleccion.getBtnAtras().addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				vista.mostrarPanel(vista.panelPresentacion);
@@ -96,38 +94,6 @@ public class Controlador {
 
 			}
 		});
-
-		/*
-		 * DefaultTableModel model = new DefaultTableModel();
-		 * System.out.println("Este es del table"+vista.eleccion.getComboBox_1().
-		 * getItemAt(0)); String ubicacion=vista.eleccion.getComboBox_1().toString();
-		 * SentenciasBBDD sentencias = new SentenciasBBDD(); ArrayList<Hotel>
-		 * hotel1=sentencias.visualizarCiudad(ubicacion);
-		 * System.out.println("table"+ubicacion); Object[] columnas = new Object[3];
-		 * columnas[0] = "Nombre"; columnas[1] = "Precio"; columnas[2]="";
-		 * model.setColumnIdentifiers(columnas); Object [] filas=new Object[2]; for(int
-		 * i=0; i<hotel1.size();i++) { filas[0] =hotel1.get(i).getNombre();
-		 * System.out.println(filas[0]); filas[1] =hotel1.get(i).getPrecio();
-		 * System.out.println(filas[1]); model.addRow(filas); }
-		 * vista.listado.getTable().setModel(model);
-		 */
-		// ---------------------------------------------------------------------------
-		/*
-		 * Object[] columnas = new Object[3]; columnas[0] = "Nombre"; columnas[1] =
-		 * "Precio"; columnas[2]=""; model.setColumnIdentifiers(columnas); Object []
-		 * filas=new Object[2]; ArrayList<Hotel> hotel1=new ArrayList<Hotel>();
-		 * SentenciasBBDD a = new SentenciasBBDD();
-		 * 
-		 * 
-		 * hotel1=a.visualizarCiudad(ubicacion);
-		 * 
-		 * for(int i=0; i<hotel1.size();i++) { filas[0] =hotel1.get(i).getNombre();
-		 * System.out.println(filas[0]); filas[1] =hotel1.get(i).getPrecio();
-		 * System.out.println(filas[1]); model.addRow(filas); } Table.setModel(model);
-		 * 
-		 * 
-		 * ScrollPane.setViewportView(Table);
-		 */
 
 		vista.Resumen.getBtnPagar().addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
